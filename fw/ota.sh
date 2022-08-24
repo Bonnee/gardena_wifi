061c9eaf321820bdca28010d58377213854f8b44
Bonnee <bonora.matteo@gmail.com>
2022-08-25 00:35:03 +0200

    Fix cycles logic.

--- fw/ota.sh
+++ fw/ota.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mqtt_broker="mqtt.bonnee.me"
+
 devname=$1
 hostname=$1
 if [ ! -z $2 ]; then
@@ -10,21 +12,21 @@ set -e
 esphome compile "$devname.yaml"
 set +e
 
-mosquitto_pub -r -h mqtt.bonnee.me -t "/$devname/esp/ota_mode" -m 'ON'
+mosquitto_pub -r -h "$mqtt_broker" -t "/$devname/esp/ota_mode" -m 'ON'
 
 echo "waiting for ping from $hostname"
-until ping -c1 "$hostname" > /dev/null 2>&1; do :; done &
-trap "kill $!; echo \"exiting\"; exit 0" INT
+until ping -c1 "$hostname" > /dev/null 2>&1; do sleep 5; :; done &
+trap "kill $!; mosquitto_pub -r -h $mqtt_broker -t "/$devname/esp/ota_mode" -m 'OFF'; echo \"exiting\"; exit 0" INT
 wait $!
 trap - INT
 
-mosquitto_pub -r -h mqtt.bonnee.me -t "/$devname/esp/ota_mode" -m 'OFF'
+mosquitto_pub -r -h "$mqtt_broker" -t "/$devname/esp/ota_mode" -m 'OFF'
 
 set -e
 esphome upload "$devname.yaml" --device "$hostname"
 
-mosquitto_pub -r -h mqtt.bonnee.me -t "/$devname/esp/sleep_mode" -m 'ON'
+mosquitto_pub -r -h "$mqtt_broker" -t "/$devname/esp/sleep_mode" -m 'ON'
 echo "putting device to sleep"
 sleep 10
-mosquitto_pub -r -h mqtt.bonnee.me -t "/$devname/esp/sleep_mode" -m 'OFF'
+mosquitto_pub -r -h "$mqtt_broker" -t "/$devname/esp/sleep_mode" -m 'OFF'
 
